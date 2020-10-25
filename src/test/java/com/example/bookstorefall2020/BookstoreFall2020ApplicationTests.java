875c9ae8b669954c1f0f8209a9a74dce7e5a4102
GoldCastillo <mikko.linna@myy.haaga-helia.fi>
2020-10-25 17:38:37 +0200

    Chapter deploy

--- src/test/java/com/example/bookstorefall2020/BookstoreFall2020ApplicationTests.java
+++ src/test/java/com/example/bookstorefall2020/BookstoreFall2020ApplicationTests.java
@@ -3,14 +3,15 @@ package com.example.bookstorefall2020;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 
 import com.example.bookstorefall2020.web.BookController;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class BookstoreFall2020ApplicationTests {
 	
