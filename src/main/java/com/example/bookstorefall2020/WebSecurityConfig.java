56eb6fb1fa34ebaae4cfb0d667507a8dc5e47520
GoldCastillo <mikko.linna@myy.haaga-helia.fi>
2020-10-26 13:10:58 +0200

    Chapter deploy

--- src/main/java/com/example/bookstorefall2020/WebSecurityConfig.java
+++ src/main/java/com/example/bookstorefall2020/WebSecurityConfig.java
@@ -1,31 +1,54 @@
 package com.example.bookstorefall2020;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import com.example.bookstorefall2020.web.UserDetailServiceImpl;
 
+ 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-	@Autowired
-	private UserDetailServiceImpl userDetailsService;
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login")
-				.defaultSuccessUrl("/booklist").permitAll().and().logout().permitAll();
-	}
-
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
-	}
+       
+@Autowired
+private UserDetailServiceImpl userDetailsService;
+       
+@Bean
+public UserDetailsService userDetailsService() {
+    return super.userDetailsService();
+}
+ 
+@Override
+protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests().antMatchers("/css/**").permitAll() //Ei vaadi sisäänkirjautumista
+                .and()
+                .authorizeRequests().anyRequest().authenticated() //kaikki muut endpointit vaativat sisäänkirjautumisen
+                .and()
+        .formLogin()
+                .loginPage("/login") //login page endpoint
+                .defaultSuccessUrl("/booklist", true) //ohjaus jos login onnistunut
+                .permitAll()
+                .and()
+        .logout()
+                .permitAll();
+}
+ 
+ 
+       
+        @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(new
+        BCryptPasswordEncoder());
+        }
+ 
 }
